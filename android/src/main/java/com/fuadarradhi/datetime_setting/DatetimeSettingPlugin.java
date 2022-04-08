2f51b07ae99d524d7a7305f8c4a9fc8466884f00
Júnior Messias <jrmessias@gmail.com>
2022-04-08 13:40:03 -0300

    :sparkles: Android V2 Embedding
    Migrate to works with Android V2 Embedding

--- android/src/main/java/com/fuadarradhi/datetime_setting/DatetimeSettingPlugin.java
+++ android/src/main/java/com/fuadarradhi/datetime_setting/DatetimeSettingPlugin.java
@@ -4,52 +4,65 @@ import android.content.Intent;
 import android.os.Build;
 import android.provider.Settings;
 
+import androidx.annotation.NonNull;
+
+import android.content.Context;
+import android.content.ContextWrapper;
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
+import io.flutter.plugin.common.BinaryMessenger;
 
-/** DatetimeSettingPlugin */
-public class DatetimeSettingPlugin implements MethodCallHandler {
-  private MethodChannel channel;
-  Registrar registrar;
+/**
+ * DatetimeSettingPlugin
+ */
+public class DatetimeSettingPlugin implements FlutterPlugin, MethodCallHandler {
+    private Context applicationContext;
+    private MethodChannel methodChannel;
 
-  DatetimeSettingPlugin(Registrar registrar){
-    this.registrar = registrar;
-  }
-
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "datetime_setting");
-    channel.setMethodCallHandler(new DatetimeSettingPlugin(registrar));
-  }
-
-  @Override
-  public void onMethodCall(MethodCall call, Result result) {
-    switch (call.method){
-      case "timeIsAuto":
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-        result.success(Settings.Global.getInt(registrar.context().getContentResolver(), Settings.Global.AUTO_TIME,0)==1);
-        }else{
-          result.success(Settings.System.getInt(registrar.context().getContentResolver(), Settings.Global.AUTO_TIME,0)==1);
-        }
-        break;
-      case "timeZoneIsAuto":
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-          result.success(Settings.Global.getInt(registrar.context().getContentResolver(), Settings.Global.AUTO_TIME_ZONE,0) == 1);
-        }else{
-          result.success(Settings.System.getInt(registrar.context().getContentResolver(), Settings.Global.AUTO_TIME_ZONE, 0) == 1);
-        }
-        break;
-
-      case "openSetting":
-        registrar.activity().startActivity(new Intent(Settings.ACTION_DATE_SETTINGS));
-        result.success(true);
-        break;
-      default:
-        result.notImplemented();
-        break;
+    @Override
+    public void onAttachedToEngine(FlutterPluginBinding binding) {
+        onAttachedToEngine(binding.getApplicationContext(), binding.getBinaryMessenger());
     }
-  }
 
+    private void onAttachedToEngine(Context applicationContext, BinaryMessenger messenger) {
+        this.applicationContext = applicationContext;
+        methodChannel = new MethodChannel(messenger, "datetime_setting");
+        methodChannel.setMethodCallHandler(this);
+    }
+
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, Result result) {
+        switch (call.method) {
+            case "timeIsAuto":
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                    result.success(Settings.Global.getInt(this.applicationContext.getContentResolver(), Settings.Global.AUTO_TIME, 0) == 1);
+                } else {
+                    result.success(Settings.System.getInt(this.applicationContext.getContentResolver(), Settings.Global.AUTO_TIME, 0) == 1);
+                }
+                break;
+
+            case "timeZoneIsAuto":
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                    result.success(Settings.Global.getInt(this.applicationContext.getContentResolver(), Settings.Global.AUTO_TIME_ZONE, 0) == 1);
+                } else {
+                    result.success(Settings.System.getInt(this.applicationContext.getContentResolver(), Settings.Global.AUTO_TIME_ZONE, 0) == 1);
+                }
+                break;
+            default:
+                result.notImplemented();
+                break;
+        }
+    }
+
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        applicationContext = null;
+        methodChannel.setMethodCallHandler(null);
+        methodChannel = null;
+    }
 }
--- example/android/app/src/main/java/com/fuadarradhi/datetime_setting_example/MainActivity.java
+++ example/android/app/src/main/java/com/fuadarradhi/datetime_setting_example/MainActivity.java
@@ -1,6 +1,8 @@
 package com.fuadarradhi.datetime_setting_example;
 
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.firebase.core.FirebaseCorePlugin;
 
 public class MainActivity extends FlutterActivity {
 }
